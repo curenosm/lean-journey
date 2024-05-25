ec163b35cbf51cb59a2cd64272170ec32b075466
Misael Cureño <curenosm@gmail.com>
2024-05-25 11:27:02 -0600

    adding greeting

---
+++ greeting/Greeting.lean
@@ -0,0 +1,9 @@
+-- This module serves as the root of the `Greeting` library.
+-- Import modules here that should be built as part of the library.
+import «Greeting».Basic
+
+-- Al utilizar guillemets
+-- Podemos usar espacios en el nombrado del directorio
+
+-- De igual manera nos permite utilizar palabras reservadas o símbolo
+-- restringidos
